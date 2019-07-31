6fdfd54cf2a9c328537c57567903b9b62b724b6b
Sureshkumar Chinnappan <32176774+schinn02@users.noreply.github.com>
2019-07-31 13:09:16 -0400

    Service Registry

--- applications/movies-ms/src/main/java/com/example/mediabase/movies/MovieMicroserviceApplication.java
+++ applications/movies-ms/src/main/java/com/example/mediabase/movies/MovieMicroserviceApplication.java
@@ -2,14 +2,14 @@ package com.example.mediabase.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 public class MovieMicroserviceApplication {
 
     public static void main(String... args) {
         SpringApplication.run(MovieMicroserviceApplication.class, args);
     }
-
 
 }
