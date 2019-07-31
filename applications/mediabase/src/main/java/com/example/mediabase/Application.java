48dee8958b47e4731da6cc31378cc2e1032c6b0b
Sureshkumar Chinnappan <32176774+schinn02@users.noreply.github.com>
2019-07-31 14:10:59 -0400

    Service Registry1D2

--- applications/mediabase/src/main/java/com/example/mediabase/Application.java
+++ applications/mediabase/src/main/java/com/example/mediabase/Application.java
@@ -5,6 +5,7 @@ import com.example.mediabase.moviesui.MovieClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import com.example.mediabase.podcastsUI.PodcastClient;
 
 @EnableEurekaClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class Application {
 
